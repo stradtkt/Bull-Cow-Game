2d7c2836acbaeecbe134d254f6f48806f133eaca
Kevin <stradtkt22@gmail.com>
2019-03-08 06:29:11 -0500

    Finished the .h file

--- FBullCowGame.h
+++ FBullCowGame.h
@@ -2,7 +2,47 @@
 // Created by Kevin on 3/6/2019.
 //
 
-#ifndef BULL_COWS_FBULLCOWGAME_H
-#define BULL_COWS_FBULLCOWGAME_H
+#pragma once
+#include <string>
 
-#endif //BULL_COWS_FBULLCOWGAME_H
+using FString = std::string;
+using int32 = int;
+
+struct FBullCowCount
+{
+    int32 Bulls = 0;
+    int32 Cows = 0;
+};
+
+enum class EGuessStatus
+{
+    Invalid_Status,
+    OK,
+    Not_Isogram,
+    Wrong_Length,
+    Not_Lowercase
+};
+
+
+class FBullCowGame
+{
+public:
+    FBullCowGame(); // constructor
+
+    int32 GetMaxTries() const;
+    int32 GetCurrentTry() const;
+    int32 GetHiddenWordLength() const;
+    bool IsGameWon() const;
+    EGuessStatus CheckGuessValidity(FString) const;
+
+    void Reset();
+    FBullCowCount SubmitValidGuess(FString);
+
+private:
+    int32 MyCurrentTry;
+    FString MyHiddenWord;
+    bool bGameIsWon;
+
+    bool IsIsogram(FString) const;
+    bool IsLowercase(FString) const;
+};
